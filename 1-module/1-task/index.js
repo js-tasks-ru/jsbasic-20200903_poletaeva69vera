45f62aa7d2634836d1ebf7e0cfb11e8aece72e00
Fida-VS <poletaeva69vera@mail.ru>
2020-09-09 11:44:51 +0300

    Решение задачи с факториалом

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,17 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
-}
+  var array = [];
+				
+  for (var i = n; i >= 1; i--) {
+    array.push(i);
+  }
+  
+  var fact = 1;
+  
+  for (var k = 0; k < array.length; k++) {
+     fact *= array[k];
+  };
+  
+  return fact;
+};
