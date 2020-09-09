98b41ad4ffa5c76a5d725ab2552a824c2d1d3d0c
Fida-VS <poletaeva69vera@mail.ru>
2020-09-09 19:10:18 +0300

    Решение задачи №3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -3,6 +3,18 @@
  * @param {string} str
  * @returns {string}
  */
-function ucFirst(str) {
-  // ваш код...
+  function ucFirst(str) { 
+    if (str.length===1){
+      return str[0].toUpperCase();
+    } else if (str===''){
+  var newSt = '';
+   return newSt;
+} else {	
+var letter = str[0].toUpperCase();
+for (var i = 1; i < str.length; i++){
+newSt = letter += str[i];
 }
+return newSt;
+}
+        }
+
