c64fecf98e67aa1789a442de29aba212fb0f5757
Fida-VS <poletaeva69vera@mail.ru>
2020-09-11 18:23:03 +0300

    Решена задача №6

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,6 +3,25 @@
  * @param {Object} salaries - объект зарплат
  * @returns {Number}
  */
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: 'December',
+  currency: 'USD',
+  isPayed: false
+};  
+
 function sumSalary(salaries) {
-  // ваш код...
+  
+  let sum = 0;
+                for (let sal of Object.values(salaries)) {
+                	if (isFinite(sal)==false){
+			 	continue;
+			}
+                	sum += sal;
+                       }
+                   
+			return sum;
+
 }
