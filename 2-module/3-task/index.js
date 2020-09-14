de2ac38d9c830e16d197dc0d254d244d3be44dac
Fida-VS <poletaeva69vera@mail.ru>
2020-09-14 11:27:06 +0300

    Калькулятор

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,20 @@
 let calculator = {
-  // ваш код
+  num1: 0,
+
+  num2: 0,
+
+  read(a, b) {
+    this.num1 = a;
+    this.num2 = b; 
+  },
+  sum(){
+    let s = this.num1 + this.num2;
+	return s;
+  },
+  mul(){
+    let m = this.num1 * this.num2;
+	return m; 
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
