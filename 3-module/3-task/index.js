8c9ec5141bd11c7a97fc6283533a228207a3d7ca
Fida-VS <poletaeva69vera@mail.ru>
2020-09-16 15:18:10 +0300

    Решение задачи №11

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,5 +3,7 @@
  * @returns {string}
  */
 function camelize(str) {
-  // ваш код...
+  var words = str.split('-');
+     var str2 = words.map((item, index) => index == 0 ? item : item[0].toUpperCase() + item.slice(1)).join('');
+     return str2;
 }
