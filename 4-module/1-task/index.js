b53a971fced418948ebf4fe4752f57a08bcd1004
Fida-VS <poletaeva69vera@mail.ru>
2020-09-22 14:37:37 +0300

    Задача 14

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,12 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+  var result = friends.map(a => a.firstName + ' ' + a.lastName);
+ var list = document.createElement('ul');
+  document.body.appendChild(list);
+ for (let i = 0; i < result.length; i++) {
+	let li = document.createElement('li');
+	li.innerHTML = result[i];
+	list.appendChild(li);
+}
 }
