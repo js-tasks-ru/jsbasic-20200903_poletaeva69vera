b8526b511345193677907bd8c122c9a01527ae37
Fida-VS <poletaeva69vera@mail.ru>
2020-09-24 12:36:54 +0300

    Задача 16

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,33 @@
  * @param {Element} table
  */
 function highlight(table) {
+    var a;
+ 
+  for (item of table.rows){
+  	
+    if (item.cells[3].hasAttribute("data-available")==false){
+    	item.setAttribute("hidden", "hidden");
+    	
+    } else if (item.cells[3].getAttribute("data-available")=="true"){
+    	item.classList.add("available");
+    	
+    } else if (item.cells[3].getAttribute("data-available")=="false"){
+    	item.classList.add("unavailable");
+    } 
+    
+    if (item.cells[2].innerHTML=="m"){
+    item.classList.add("male");	
+    
+    } else if(item.cells[2].innerHTML=="f"){
+    	item.classList.add("female");	
+    	
+    }
+    
+    a = +item.cells[1].innerHTML;
+    
+    if(a < 18){
+    	item.setAttribute("style", "text-decoration: line-through;");
+    }
+ };
+ return table;
 }
