932ed4c604534099575d8c5171a9edacf65bf747
Fida-VS <poletaeva69vera@mail.ru>
2020-09-24 19:13:08 +0300

    Задача 18

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,20 @@
 function toggleText() {
-  // ваш код...
+
+  let button = document.querySelector('.toggle-text-button');
+
+  let txt = document.getElementById('text');
+
+  button.addEventListener("click", f);
+
+  function f(){
+
+    button.classList.toggle('active');
+
+    if (button.classList.contains("active")){
+      txt.setAttribute("hidden", "hidden");
+      
+     } 	else if (button.classList.contains("active")!==true){
+      txt.removeAttribute("hidden", "hidden");
+     }
+ }
 }
