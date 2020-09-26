0f11843572c00d1581ec3b1e85dc17e8f3cfb3f2
Fida-VS <poletaeva69vera@mail.ru>
2020-09-26 15:53:30 +0300

    проверка кнопок

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,21 @@
 function initCarousel() {
-  // ваш код...
+  let btnLeft = document.querySelector('.carousel__arrow_left');
+  let btnRight = document.querySelector('.carousel__arrow_right');
+  let innerBox = document.querySelector('.carousel__inner');
+  let container = document.querySelector('.container');
+  let slides = document.querySelectorAll('.carousel__slide');
+  let photo = document.querySelector('.carousel__img');
+
+  container.addEventListener("click", leaf);
+ 
+  function leaf(event){
+    let target = event.target; 
+   
+
+    if (target==btnLeft){
+     alert('hello');
+    } else if (target==btnRight){
+     alert('user');
+    } else if(target!==btnLeft&&btnRight) return;
+  }
 }
